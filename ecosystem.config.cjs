3bd2fdfd9d940c63cf4da6163eaa2fa21c00d7aa
cranchagency <artemevtimur.main@gmail.com>
2025-04-14 04:11:50 +0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,6 +6,8 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    node_args: '--enable-source-maps',
+    interpreter: 'node',
     env: {
       NODE_ENV: 'production',
       PORT: 3000
